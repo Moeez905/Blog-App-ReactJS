99bc107774784ca7560ccc45eeb75302693f24ae
Moeez905 <x311099@gmail.com>
2023-06-14 11:22:02 +0500

    revised removal

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,15 +1,10 @@
 import "./details.css";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchPosts } from "../../actions/blogPosts";
 
-
-
-
 function Details() {
   const { id } = useParams();
-  const dispatch = useDispatch();
   const posts = useSelector((state) => state.blogPosts.posts);
   const post = posts.find((post) => post.id === parseInt(id));
 
